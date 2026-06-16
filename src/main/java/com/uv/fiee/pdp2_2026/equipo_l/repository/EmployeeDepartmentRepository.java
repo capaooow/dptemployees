51fdf93f86e934f289689a0953f8e0e300e4815b
capaooow <paolawitrago@outlook.com>
2026-06-15 21:18:42 -0600

    update: ahora sí, creación del repositorio del EmployeeDepartment.

---
+++ src/main/java/com/uv/fiee/pdp2_2026/equipo_l/repository/EmployeeDepartmentRepository.java
@@ -0,0 +1,22 @@
+package com.uv.fiee.pdp2_2026.equipo_l.repository;
+
+import com.uv.fiee.pdp2_2026.equipo_l.model.EmployeeDepartment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+/*
+ *  Repositorio encargado de las consultas a la vista vEmployeeDepartment.
+ */
+
+@Repository
+public interface EmployeeDepartmentRepository extends JpaRepository<EmployeeDepartment, Integer> {
+  /*
+   * Busca y retorna la lista de empleados filtrados por su departamento.
+   * 
+   * @param department: Nombre del departamento.
+   * 
+   * @return: Una colección List con los empleados encontrados.
+   */
+  List<EmployeeDepartment> ffindByDepartment(String department);
+}
