32d29ad640d69377e1d105232f607429d3759804
capaooow <paolawitrago@outlook.com>
2026-06-15 18:36:20 -0600

    update: actualización del app como springbootapplication.

--- src/main/java/com/uv/fiee/pdp2_2026/equipo_l/App.java
+++ src/main/java/com/uv/fiee/pdp2_2026/equipo_l/App.java
@@ -1,13 +1,18 @@
 package com.uv.fiee.pdp2_2026.equipo_l;
 
-/**
- * Hello world!
- *
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Clase principal de inicialización de Spring Boot.
+ * Desde aquí se levanta el servidor web embebido (Tomcat) en el puerto 8080.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    // Le dice a Spring que arranque la aplicación usando esta misma clase como
+    // base.
+    SpringApplication.run(App.class, args);
+  }
 }
