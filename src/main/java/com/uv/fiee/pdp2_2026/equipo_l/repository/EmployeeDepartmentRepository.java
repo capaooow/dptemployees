56d6905501d1fb78bc3f1412a1794e67292cafc8
capaooow <paolawitrago@outlook.com>
2026-06-17 14:58:32 -0600

    update: Se actualizó el repository para añadir un query.

--- src/main/java/com/uv/fiee/pdp2_2026/equipo_l/repository/EmployeeDepartmentRepository.java
+++ src/main/java/com/uv/fiee/pdp2_2026/equipo_l/repository/EmployeeDepartmentRepository.java
@@ -1,7 +1,9 @@
 package com.uv.fiee.pdp2_2026.equipo_l.repository;
 
 import com.uv.fiee.pdp2_2026.equipo_l.model.EmployeeDepartment;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -18,5 +20,13 @@ public interface EmployeeDepartmentRepository extends JpaRepository<EmployeeDepa
    * 
    * @return: Una colección List con los empleados encontrados.
    */
-  List<EmployeeDepartment> ffindByDepartment(String department);
+  List<EmployeeDepartment> findByDepartment(String department);
+
+  /*
+   * Busca y retorna la lista de departamentos que existen.
+   * 
+   * @return: Una colección List con los departamentos encontrados.
+   */
+  @Query("SELECT DISTINCT e.department FROM EmployeeDepartment e")
+  List<String> findDistinctDepartments();
 }
